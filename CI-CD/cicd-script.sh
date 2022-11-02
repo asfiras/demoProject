baf198706db4861b3264017769742f0ab612ba97
Firas <asfiras@hotmail.com>
2022-11-02 20:41:03 +0100

    add test case to cisd and finilize the project

--- CI-CD/cicd-script.sh
+++ CI-CD/cicd-script.sh
@@ -2,7 +2,7 @@
 
 #Deploy First App
 cd /home/firas/Firas/demoProject/first-app
- mvn clean compile assembly:single
+mvn clean compile assembly:single
 STATUS=$?
 if [ $STATUS -eq 0 ]; then
 echo "App1 Deployment Successful"
@@ -18,6 +18,15 @@ if [ $STATUS -eq 0 ]; then
 echo "App2 Deployment Successful"
 else
 echo "App2 Deployment Failed"
+fi
+
+#test main App function by sending test massage and test to recieve it
+awslocal sqs send-message --queue-url http://localhost:4566/000000000000/App1_q --message-body "test1234" --delay-seconds 10
+check_string=$(awslocal sqs receive-message --queue-url http://localhost:4566/000000000000/App1_q | jq -r '.Messages[] | .Body')
+if [ $check_string == "test1234" ]; then
+echo "App Function tested Successful"
+else
+echo "App Test Failed"
 fi
 
 #copy artifacts to S3
