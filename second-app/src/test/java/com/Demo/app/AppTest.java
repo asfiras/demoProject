e7013a2ba2a322e1a9c7dbb6830ab1446eab9c1d
Firas <asfiras@hotmail.com>
2022-11-05 11:08:44 +0100

    add some comment and junit test

--- second-app/src/test/java/com/Demo/app/AppTest.java
+++ second-app/src/test/java/com/Demo/app/AppTest.java
@@ -1,7 +1,15 @@
 package com.Demo.app;
 
-import static org.junit.Assert.assertTrue;
-
+import org.junit.Assert;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import com.amazonaws.services.sqs.model.Message;
+import com.amazonaws.services.sqs.model.SendMessageRequest;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 import org.junit.Test;
 
 /**
@@ -11,10 +19,35 @@ public class AppTest
 {
     /**
      * Rigorous Test :-)
+     * @throws InterruptedException
      */
     @Test
-    public void shouldAnswerWithTrue()
+    public void mainTest()
     {
-        assertTrue( true );
+         //configure localstack access
+         BasicAWSCredentials awsCreds = new BasicAWSCredentials("test", "test");
+         String output="";
+         final AmazonSQS sqs = AmazonSQSClientBuilder.standard()
+         .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://172.17.0.2:4566","us-east-1"))
+         .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
+         .build();
+        SendMessageRequest send_msg_request = new SendMessageRequest()
+        .withQueueUrl("http://172.17.0.2:4566/000000000000/App2_q")
+        .withMessageBody("TeSt")
+        .withDelaySeconds(5);
+        try {
+        TimeUnit.SECONDS.sleep(7);
+    } catch (InterruptedException e) {
+
+        e.printStackTrace();
+    }
+        sqs.sendMessage(send_msg_request);
+        List<Message> messages = sqs.receiveMessage("http://172.17.0.2:4566/000000000000/App2_q").getMessages();
+                System.out.println("the massages you recieved are:");
+                for (Message m : messages) {
+                    output=m.getBody();
+                    sqs.deleteMessage("http://172.17.0.2:4566/000000000000/App2_q", m.getReceiptHandle());
+                }
+                Assert.assertEquals(output, "TeSt");
     }
 }
